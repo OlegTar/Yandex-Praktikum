d8fc0b446473cf5dcde15af9feaa4d26b5c49785
Oleg <ot88899888@gmail.com>
2026-02-06 23:43:01 +0300

    Create Solution.java

---
+++ Спринт 2/Список дел/Solution.java
@@ -0,0 +1,43 @@
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
+// <template>
+class Node<V> {  
+    public V value;  
+    public Node<V> next;  
+ 
+    public Node(V value, Node<V> next) {  
+        this.value = value;  
+        this.next = next;  
+    }  
+}
+// <template>
+
+public class Solution {
+    public static void solution(Node<String> head) {
+        // Your code
+        // ヽ(´▽`)/
+    	PrintWriter writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+        while (head != null) {
+            writer.println(head.value);
+            head = head.next;
+        }
+        writer.flush();
+    }
+
+    private static void test() {
+        Node<String> node3 = new Node<>("node3", null);
+        Node<String> node2 = new Node<>("node2", node3);
+        Node<String> node1 = new Node<>("node1", node2);
+        Node<String> node0 = new Node<>("node0", node1);
+        solution(node0);
+        /*
+        Output is:
+        node0
+        node1
+        node2
+        node3
+        */
+    }
+}
