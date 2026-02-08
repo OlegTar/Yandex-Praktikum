4dc43dbeccf7b314b34af83238275ae02c9bf075
Oleg <ot88899888@gmail.com>
2026-02-08 20:46:12 +0300

    Create Program.java

---
+++ Спринт 8/G. Поиск со сдвигом/Program.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] x = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            x[i] = scanner.nextInt();
+        }
+
+        int m = scanner.nextInt();
+        int[] a = new int[m];
+
+        for (int i = 0; i < m; i++) {
+            a[i] = scanner.nextInt();
+        }
+
+        if (a.length > x.length) {
+            return;
+        }
+
+        List<Integer> answer = new ArrayList<>();
+        int end = x.length - a.length;
+
+        for (int i = 0; i <= end; i++) {
+            boolean isMatch = true;
+            int c = x[i] - a[0];
+            for (int j = 1; j < a.length; j++) {
+                if (x[i + j] != a[j] + c) {
+                    isMatch = false;
+                    break;
+                }
+            }
+
+            if (isMatch) {
+                answer.add(i + 1);
+            }
+        }
+
+        for (int idx : answer) {
+            System.out.printf("%d ", idx);
+        }
+        System.out.println();
+    }
+}
